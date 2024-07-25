001f7d843e30cabc510140b8244a495f56a825cc
Sanchit <sanchit1417@gmail.com>
2024-07-25 23:48:28 +0530

    backedn front end

---
+++ backend/db.js
@@ -0,0 +1,38 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Sanchit:sanchit%4017@cluster0.8djn6b3.mongodb.net/paytm');
+
+const UserSchema = new mongoose.Schema({
+    username : {
+        type :String,
+        required : true,
+        trim : true,
+        lowercase : true,
+        maxLength : 50
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 6,
+        maxLength : 50
+    },
+    fistName : {
+        type :String,
+        trim : true,
+        lowercase : true,
+        maxLength : 50
+    },
+    lastName : {
+        type :String,
+        trim : true,
+        lowercase : true,
+        maxLength : 50
+    }
+})
+
+const User = mongoose.model('User',UserSchema);
+
+module.exports ={
+    User
+};
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+
+const app = express();
+const PORT = 3030;
+
+const router = express.Router();
+
+module.exports = router;
