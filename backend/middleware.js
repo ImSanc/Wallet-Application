70a0457e0b7c0b36110cd5486b18d94f5a059f13
Sanchit <sanchit1417@gmail.com>
2024-07-29 19:27:43 +0530

    Middle ware refactro

--- backend/middleware.js
+++ backend/middleware.js
@@ -3,27 +3,27 @@ const {jwtToken} = require("./config.js")
 
 function  authMiddleWare( request,response,next)
 {
-    console.log("middleware");
     const {authorization} = request.headers;
-    console.log(authorization);
+
     if(!authorization){
-        console.log("1");
         return response.status(401).json({message : "Authorization header is missing"})
     }
 
     const token = authorization.split(' ')[1];
 
     if(!token){
-        console.log("2");
         return response.status(401).json({message : "Authorization header is missing"})
+    }
+    try{
+    const decoded = jsonwebtoken.verify(token,jwtToken);
+    request.username = decoded.username;
+    next();
+    }
+    catch (err){
+        return res.status(403).json({});
     }
 
 
-    console.log(jwtToken);
-    const decode = jsonwebtoken.verify(token,jwtToken);
-
-    console.log(decode);
-    next();
 }
 
 module.exports = {authMiddleWare}
